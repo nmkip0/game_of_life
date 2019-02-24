d36e0874c7252f2e0db3a64945ac18235c8f393e
nmkip <juan.ignacio848@gmail.com>
2019-02-24 19:31:22 -0300

    Restart

--- src/test/java/com/nmkip/gameoflife/GameShould.java
+++ src/test/java/com/nmkip/gameoflife/GameShould.java
@@ -7,11 +7,4 @@ import static org.junit.Assert.assertThat;
 
 class GameShould {
 
-    @Test
-    void initialise_system() {
-        Game game = new Game(Cell.at(0, 0), Cell.at(0, 1));
-
-        assertThat(game, is(new Game(Cell.at(0, 0), Cell.at(0, 1))));
-    }
-
 }
